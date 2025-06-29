b273a988301b2fe84d90e93367d62e1da0361b67
nefiodova <an.nefd@yandex.com>
2025-06-29 15:47:06 +0300

    added SoftAssertionshaveJUnit5

---
+++ src/test/java/Homework4.java
@@ -0,0 +1,51 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Homework4 {
+
+
+    @BeforeAll
+    static void testBrowseConfiguration() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+
+    @Test
+    void SoftAssertionshaveJUnit5() {
+
+        //Открыть Selenide на GitHub
+        open("/selenide/selenide");
+
+        //Переходим в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        //Наличие SoftAssertions
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+
+        //Открыть страницу SoftAssertions
+        $("#wiki-body").$(byText("Soft assertions")).click();
+
+        //Наличие примера кода для JUnit
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+    }
+}
